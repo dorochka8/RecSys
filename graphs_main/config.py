f9890562a82472b62d79da39fcd3465f5ce8a8d7
dora <97133490+dorochka8@users.noreply.github.com>
2023-12-25 21:21:07 +0300

    Add files via upload

---
+++ graphs_main/config.py
@@ -0,0 +1,15 @@
+config = {
+  # general
+  'top_k'                : 10,
+
+  # graph model set-up
+  'graph_lr'             : 1e-2,
+  'graph_epochs'         : 10,
+  'graph_lr_decay_step'  : 5,
+  'graph_lr_decay_value' : 10, 
+  'graph_alpha'          : 0.2,
+  'graph_dropout'        : 0.8, 
+  'graph_weight_decay'   : 0.00,
+
+
+}
