6f4da3ea002a376af07e955f38f643c43e069c18
dora <97133490+dorochka8@users.noreply.github.com>
2023-12-25 18:30:29 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+config = {
+  # general
+  'top_k'                : 10,
+
+  # graph model set-up
+  'graph_lr'             : 1e-3,
+  'graph_epochs'         : 8,
+  'graph_lr_decay_step'  : 20,
+  'graph_lr_decay_value' : 10, 
+  'graph_alpha'          : 0.2,
+  'graph_dropout'        : 0.6, 
+
+
+}
