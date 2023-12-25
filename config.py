25641d22e236d50c5ea41c20c31f003ae400e55e
dora <97133490+dorochka8@users.noreply.github.com>
2023-12-25 18:41:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ config = {
   # graph model set-up
   'graph_lr'             : 1e-3,
   'graph_epochs'         : 8,
-  'graph_lr_decay_step'  : 20,
-  'graph_lr_decay_value' : 10, 
   'graph_alpha'          : 0.2,
   'graph_dropout'        : 0.6, 
 
